4051ca3658e5369944df35511910e3704f84f889
twdeveloper <twdeveloper@outlook.com>
2024-02-26 14:46:28 +0800

    v1

---
+++ EchoBot2/Bots/ChatGPT.cs
@@ -0,0 +1,113 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using EchoBot2.Bots;
+using static EchoBot1.Bots.ChatGPT;
+using System.Collections.Generic;
+
+namespace EchoBot1.Bots
+{
+    public class ChatGPT
+    {
+        const string AzureOpenAIEndpoint = "https://👉endpoint.openai.azure.com";  //👉replace it with your Azure OpenAI Endpoint
+        const string AzureOpenAIModelName = "👉deployment"; //👉repleace it with your Azure OpenAI Model Name
+        const string AzureOpenAIToken = "👉apikey"; //👉repleace it with your Azure OpenAI Token
+        const string AzureOpenAIVersion = "2024-02-15-preview";  //👉replace  it with your Azure OpenAI Model Version
+
+        [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+        public enum role
+        {
+            assistant, user, system
+        }
+
+        public static string CallAzureOpenAIChatAPI(
+            string endpoint, string modelName, string apiKey, string apiVersion, object requestData)
+        {
+            var client = new HttpClient();
+
+            // 設定 API 網址
+            var apiUrl = $"{endpoint}/openai/deployments/{modelName}/chat/completions?api-version={apiVersion}";
+
+            // 設定 HTTP request headers
+            client.DefaultRequestHeaders.Add("api-key", apiKey);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT heade
+            // 將 requestData 物件序列化成 JSON 字串
+            string jsonRequestData = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
+            // 建立 HTTP request 內容
+            var content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json");
+            // 傳送 HTTP POST request
+            var response = client.PostAsync(apiUrl, content).Result;
+            // 取得 HTTP response 內容
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(responseContent);
+            return obj.choices[0].message.content.Value;
+        }
+
+
+        public static string getResponseFromGPT(string Message, List<Message> chatHistory)
+        {
+            //建立對話紀錄
+            var messages = new List<ChatMessage>
+                    {
+                        new ChatMessage {
+                            role = Role.system ,
+                            content = @"
+                                假設你是一個專業的導遊人員，對於客戶非常有禮貌、也能夠安撫客戶的抱怨情緒。
+                                請檢視底下的客戶訊息，以最親切有禮的方式回應。
+
+                                但回應時，請注意以下幾點:
+                                * 不要說 '感謝你的來信' 之類的話，因為客戶是從對談視窗輸入訊息的，不是寫信來的
+                                * 不能過度承諾
+                                * 要同理客戶的情緒
+                                * 要能夠盡量解決客戶的問題
+                                * 不要以回覆信件的格式書寫，請直接提供對談機器人可以直接給客戶的回覆
+                                ----------------------
+"
+                        }
+                    };
+
+            //添加歷史對話紀錄
+            foreach (var HistoryMessageItem in chatHistory)
+            {
+                //添加一組對話紀錄
+                messages.Add(new ChatMessage()
+                {
+                    role = Role.user,
+                    content = HistoryMessageItem.UserMessage
+                });
+                messages.Add(new ChatMessage()
+                {
+                    role = Role.assistant,
+                    content = HistoryMessageItem.ResponseMessage
+                });
+            }
+            messages.Add(new ChatMessage()
+            {
+                role = Role.user,
+                content = Message
+            });
+            //回傳呼叫結果
+            return ChatGPT.CallAzureOpenAIChatAPI(
+               AzureOpenAIEndpoint, AzureOpenAIModelName, AzureOpenAIToken, AzureOpenAIVersion,
+                new
+                {
+                    model = "gpt-3.5-turbo",
+                    messages = messages
+                }
+             );
+        }
+    }
+
+    public class ChatMessage
+    {
+        public Role role { get; set; }
+        public string content { get; set; }
+    }
+
+    [JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
+    public enum Role
+    {
+        assistant, user, system
+    }
+}
---
+++ EchoBot2/Bots/EchoBot.cs
@@ -0,0 +1,50 @@
+﻿// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.22.0
+
+using EchoBot1.Bots;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot2.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var responseMsg = "";
+
+            if (turnContext.Activity.Text.Contains("/reset"))
+            {
+                ChatHistoryManager.DeleteIsolatedStorageFile();
+                responseMsg = "我已經把之前的對談都給忘了!";
+            }
+            else
+            {
+                var chatHistory = ChatHistoryManager.GetMessagesFromIsolatedStorage("UserA");
+                //var replyText = $"Echo: {turnContext.Activity.Text}";
+                responseMsg = ChatGPT.getResponseFromGPT(turnContext.Activity.Text, chatHistory);
+                //儲存聊天紀錄
+                ChatHistoryManager.SaveMessageToIsolatedStorage(
+                    DateTime.Now, "UserA", turnContext.Activity.Text, responseMsg);
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(responseMsg, responseMsg), cancellationToken);
+
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
